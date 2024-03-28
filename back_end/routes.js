68cd0008ac24e8b8c255d0c6aaa04d7f3aae8950
Aliceluis <128178161+Aliceluis@users.noreply.github.com>
2024-03-28 10:03:29 -0300

    Add files via upload

---
+++ back_end/routes.js
@@ -0,0 +1,37 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const chamados = await getAllChamados();
+    return res.json(chamados);
+  } 
+  catch (error) {
+   return res.status(500).json( 'algo deu errado');
+  }
+});
+
+router.get('/routes/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    await pool.query`insert into Produtos values(${descricao},${id})`
+    return res.status(201).json(`ok`)
+  } 
+  catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
+router.post('/server', async (req, res) => {
+  try {
+    const { data, nomeCliente, descricao } = req.body;
+    await pool.query`update Produtos set data = ${data}, nome = ${nome} where id = ${id}`
+    return res.status(201).json('atualizado')
+  }
+   catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+export default router
